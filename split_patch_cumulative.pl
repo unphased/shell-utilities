b99d509b7b0932f9bc74ac5665483c18f12b6b30
Steven Lu <stevenlu443@gmail.com>
2013-04-22 18:27:09 -0400

    ok

--- split_patch_cumulative.pl
+++ split_patch_cumulative.pl
@@ -3,17 +3,17 @@
 use warnings;
 use strict;
 
-# splits a diff (patch) at all hunk boundaries, for use in automatic binary search
-# uses much more disk than the incremental version but is less complex.
+# Splits a diff (patch) at all hunk boundaries, for use in automatic binary search.
+# Uses much more disk than the incremental version but is less complex.
 
 my $diff = '';
 my $chunks = 0;
 
-qx/rm -v .tmp_split_patch_cumulative .tmp_spc_*/;
+qx{rm ~/.tmp/.tmp*};
 
 while (<STDIN>) {
 	if (/^\d+(?:,\d+)?c\d+(?:,\d+)?$/) {
-		open(my $FILE, ">>.tmp_spc_$chunks");
+		open(my $FILE, ">>~/.tmp/.tmp_spc_$chunks");
 		print $FILE $diff;
 		close($FILE);
 		$chunks++;
@@ -21,14 +21,37 @@ while (<STDIN>) {
 	$diff .= $_;
 }
 
-open(my $FILE, ">>.tmp_split_patch_cumulative");
+open(my $FILE, ">>~/.tmp/.tmp_split_patch_cumulative");
 print $FILE $diff;
 close($FILE);
-
 # Finished generating intermediate patches.
 
 # Back up the source file
+qx{cp $1 ~/.tmp/.tmp_spc_sourcefile};
 
+# dry-run with patch on target file.
+qx{patch --dry-run ~/.tmp/.tmp_spc_sourcefile ~/.tmp/.tmp_split_patch_cumulative};
+die if $?;
 
 # Perform binary search over the hunks (as there is no way of knowing which hunks were made at what time chronologically)
+for ()
 
+sub run {
+	my ($index) = @_;
+	qx{patch };
+
+	my $goodbad;
+	while (!valid_gb($goodbad)) {
+		print '\nTest! good or bad? ';
+		readline $goodbad;
+	}
+	if (substr($goodbad,0,1) eq 'g') {
+
+	} else {
+
+	}
+}
+
+sub valid_gb {
+	return $_ eq 'good' || $_ eq 'bad' || $_ eq 'g' || $_ eq 'b';
+}

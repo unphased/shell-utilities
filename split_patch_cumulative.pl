db1215549c302a275e16a4f4748e74e533449bed
Steven Lu <stevenlu443@gmail.com>
2013-04-23 13:20:49 -0400

    Comlpeting the patch-binsearcher. Realized I didn't need it for this particular task

--- split_patch_cumulative.pl
+++ split_patch_cumulative.pl
@@ -4,7 +4,12 @@ use warnings;
 use strict;
 
 # Splits a diff (patch) at all hunk boundaries, for use in automatic binary search.
-# Uses much more disk than the incremental version but is less complex.
+# Uses much more disk than the proper incremental diff way but is less complex.
+# This splits a single patch into as many versions of the file as there are hunks
+# in the patch. It is important to realize that this is only practical in a
+# "needle in haystack" situation where the changes are all small & independent. In typical
+# situations most of these intermediately generated versions would be completely
+# non-functioning code and the binary search bug assumptions are not likely to hold
 
 my $diff = '';
 my $chunks = 0;
@@ -30,25 +35,41 @@ close($FILE);
 qx{cp $1 ~/.tmp/.tmp_spc_sourcefile};
 
 # dry-run with patch on target file.
+print "executing dry-run on patch.";
 qx{patch --dry-run ~/.tmp/.tmp_spc_sourcefile ~/.tmp/.tmp_split_patch_cumulative};
 die if $?;
 
 # Perform binary search over the hunks (as there is no way of knowing which hunks were made at what time chronologically)
-for ()
+my $upper_bound = $chunks
+my $lower_bound = 0;
+my $cur;
+while (1) {
+	if ($upper_bound - $lower_bound <= 1) {
+		print "Last known good version is $lower_bound";
+		last;
+	}
+	$cur = int(($lower_bound + $upper_bound)/ 2);
+	print "Testing $cur now.";
+	if (run($cur)) {
+		$lower_bound = $cur;
+	} else {
+		$upper_bound = $cur;
+	}
+}
 
 sub run {
 	my ($index) = @_;
-	qx{patch };
+	qx{patch -o $1 ~/.tmp/.tmp_spc_sourcefile ~/.tmp/.tmp_spc_$index}; # obtain and set the $index-th patch
 
 	my $goodbad;
 	while (!valid_gb($goodbad)) {
-		print '\nTest! good or bad? ';
+		print '\nTest! good or bad?';
 		readline $goodbad;
 	}
 	if (substr($goodbad,0,1) eq 'g') {
-
+		return 1;
 	} else {
-
+		return 0;
 	}
 }
 
